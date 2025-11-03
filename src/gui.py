655aabf6f7af2908b09973c0e863938430bd4a6e
IStealMoney <IStealMoney@users.noreply.github.com>
2025-11-03 22:38:51 +0100

    created GUI

---
+++ src/gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("KMeansClustering")
+root.configure(background="grey")
+root.minsize(800, 800)
+root.maxsize(1000, 1000)
+root.geometry("800x800+100+100")
+
+root.mainloop()

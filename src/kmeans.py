bba7ef021094b75ea1d4eb4e71399c0ed3b30916
IStealMoney <IStealMoney@users.noreply.github.com>
2025-11-08 20:58:09 +0100

    declared functions

--- src/kmeans.py
+++ src/kmeans.py
@@ -3,26 +3,42 @@ import sys
 cluster = []
 k = 3
 
-def initCluster():
-    global cluster
-    cluster = []
-    for i in range(k):
-        cluster.append([])  # cluster initialisieren
+class KMeansClustering:
 
-def distanz(a, b):
-    deltaX = a[0] - b[0]
-    deltaY = a[1] - b[1]
-    return pow(pow(deltaX, 2) + pow(deltaY,2), 0.5)
+    def __init__(self): # constructor
+        self.data = data
 
-def datenpunktZuordnen(data):
-    global cluster
-    minDist = sys.float_info.max
-    clusterId = -1
+    def ermittleExtremwert(self):
 
-    for i in range(k):
-        d = distanz(data, cluster[i][0])  # mit mittelpunkten vergleichen
-        if d < minDist:
-            minDist = d
-            clusterId = i
+    def zentrenZufaelligSetzen(self):
 
-    cluster[clusterId].append(data)
+    def datenpunkteZuordnen(self):
+
+    def datenpunktZuordnen(data):
+        global cluster
+        minDist = sys.float_info.max
+        clusterId = -1
+
+        for i in range(k):
+            d = distanz(data, cluster[i][0])  # mit mittelpunkten vergleichen
+            if d < minDist:
+                minDist = d
+                clusterId = i
+
+        cluster[clusterId].append(data)
+
+    def initCluster(self):
+        global cluster
+        cluster = []
+        for i in range(k):
+            cluster.append([])  # cluster initialisieren
+
+    def distanz(a, b):
+        deltaX = a[0] - b[0]
+        deltaY = a[1] - b[1]
+        return pow(pow(deltaX, 2) + pow(deltaY,2), 0.5)
+
+    def zentrenNeuBerechnen(self):
+
+    def mittelwert(cluster):
+        return
